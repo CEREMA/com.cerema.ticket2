cd23a0423aab98bd992fa70a161322c8cb236c8b
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-09 14:25:07 +0100

    Update# 72

--- src/Contents/Auth/Officer.js
+++ src/Contents/Auth/Officer.js
@@ -2,59 +2,26 @@ Officer = {
 	login : function(profile,auth_type,cb)
 	{
 		if (auth_type=="cas") {
-			/*
-			profile.username
-			*/
 			if (!profile.username) cb({});
 			var mail=profile.username.toLowerCase();
-			Officer.using('db').store('xxx','xxx where mail="'+mail+'")',function(err,result){
+			Officer.using('db').store('bpclight','select agents.kage,agents.kuni,agents.ksub,nom,prenom,unites.libunic,unites.kets,subdis.libsubc from agents join unites on unites.kuni=agents.kuni join subdis on subdis.ksub=agents.ksub where agents.kage in (select kage from mela where libmela="'+mail+'")',function(err,result){
 				if (!err) {
 					var response={
 						lastname: result.data[0].nom,
 						firstname: result.data[0].prenom,
-						uid: result.data[0].uid,
-						mail: mail
+						uid: result.data[0].kage,
+						mail: mail,
+						kets: result.data[0].kets,
+						kuni: result.data[0].kuni,
+						ksub: result.data[0].ksub,
+						libunic: result.data[0].libunic,
+						libsubc: result.data[0].libsubc,
+						profiles: Officer.getProfile(mail.split('@')[0])
 					};
-					cb(response);
+					cb(response);			
 				} else cb(err);
 			});
 		};
-		if (auth_type=="google") {
-			/* 
-			profile.id
-			profile.email
-			profile.verified_email
-			profile.name
-			profile.given_name
-			profile.family_name
-			profile.link
-			profile.gender
-			profile.locale
-			*/
-			profile=profile.username;
-			var mail=profile.email;
-			Officer.using('db').store('xxx','xxxx where login="'+mail+'"',function(err,result){
-
-				var response={
-					lastname: profile.family_name,
-					firstname: profile.given_name,
-					lang: profile.locale,
-					mail: mail
-				};
-				
-				if (result.data.length>0) {
-					// account already exists in our database
-					response.member=1;
-					response.uid=result.data[0].kage;
-					cb(response);
-				} else {
-					// we create account...
-					
-				};
-				
-			});
-			
-		}
 		
 	}
 };
