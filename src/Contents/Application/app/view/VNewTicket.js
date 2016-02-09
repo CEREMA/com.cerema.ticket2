cc30b1568ef8d6607624e2b11afda8b089707ff3
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-09 16:33:42 +0100

    Update# 102

---
+++ src/Contents/Application/app/view/VNewTicket.js
@@ -0,0 +1,27 @@
+App.view.define('VNewTicket', {
+    extend: "Ext.window.Window",
+    alias: 'widget.VNewTicket',
+    initComponent: function() {
+        this.width = 1024;
+        this.height = 660;
+
+        this.layout = {
+            type: 'vbox'
+        };
+
+        this.bbar = [
+        ];
+
+        this.tbar = [
+        ];
+		
+        this.defaults = {
+            split: true
+        };
+
+        this.items = [
+		];
+
+        this.callParent();
+    }
+});
