14c80562f8f22b16856101b572372117d6acdd27
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-11 19:36:53 +0100

    Update# 226

--- src/Contents/Services/Demandes.js
+++ src/Contents/Services/Demandes.js
@@ -111,6 +111,31 @@ Demandes = {
 			return;
 		}
 	},
+	myclitickets: function(o,cb) {
+		var db=Demandes.using('db');
+        
+		if (o.profil.indexOf("SII")>-1) {
+			// profil SII, voit tout le monde
+			db.model('infocentre','select * from ticket where cli='+o.id+' and state<>-1 and state<5 order by date_depot desc',cb);
+			return;
+		};
+		if (o.profil.indexOf("GEST")>-1) {
+			// profil SII, voit tout le monde
+			db.model('infocentre','select * from ticket where cli='+o.id+' and state<>-1 and state<5 order by date_depot desc',cb);
+			return;
+		};
+		if (o.profil.indexOf("CLI")>-1) {
+			// profil CLI ne voit que les agents de son département
+			db.query('bpclight',"select kage from agents where kuni in (select kuni from agents where kage='"+o.uid+"') and agents.actif=1", function(e,r) {
+				var KAGE=[];
+				for (var i=0;i<r.length;i++) {
+					KAGE.push(r[i].kage);
+				};
+				db.model('infocentre','select * from ticket where agent in ('+KAGE.join(',')+') and state<>-1 and state<5 and cli='+o.id+' order by date_depot desc',cb);
+			});
+			return;
+		}
+	},
 	mytickets: function(o,cb) {
 		var db=Demandes.using('db');
         
