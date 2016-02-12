2b26c542e3d5a2d894562d85443a7d595128683f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-02-12 16:12:49 +0100

    Update# 286

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -144,6 +144,7 @@ App.controller.define('CMain', {
             App.DB.get('infocentre://ticket?id='+p.record.id,p,function(r){
                 var html=[];
                 html.push("Agent concerné :<br><b>"+r.data[0].agent_nom+"</b><br>");
+                alert(html.join(''));
                 App.get(p,"panel#sumup").update(html.join(''));
                 //App.get(p,"panel#cli").update("<b>Déposé par "+r.data[0].cli_nom+'</b>');
             });  
