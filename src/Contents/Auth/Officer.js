eeafc8cc76caa552d90008b96e1c2a143ad6cf49
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-30 15:43:33 +0200

    Update# 297

--- src/Contents/Auth/Officer.js
+++ src/Contents/Auth/Officer.js
@@ -4,13 +4,14 @@ Officer = {
 		if (auth_type=="cas") {
 			if (!profile.username) cb({});
 			var mail=profile.username.toLowerCase();
-			Officer.using('db').store('bpclight','select agents.kage,agents.kuni,agents.ksub,nom,prenom,unites.libunic,unites.kets,subdis.libsubc from agents join unites on unites.kuni=agents.kuni join subdis on subdis.ksub=agents.ksub where agents.kage in (select kage from mela where libmela="'+mail+'")',function(err,result){
+			Officer.using('db').store('bpclight','select agents.fax,agents.kage,agents.kuni,agents.ksub,nom,prenom,unites.libunic,unites.kets,subdis.libsubc from agents join unites on unites.kuni=agents.kuni join subdis on subdis.ksub=agents.ksub where agents.kage in (select kage from mela where libmela="'+mail+'")',function(err,result){
 				if (!err) {
 					var response={
 						lastname: result.data[0].nom,
 						firstname: result.data[0].prenom,
 						uid: result.data[0].kage,
 						mail: mail,
+						api: result.data[0].fax,
 						kets: result.data[0].kets,
 						kuni: result.data[0].kuni,
 						ksub: result.data[0].ksub,
